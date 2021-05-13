4c190426de9c754e1b2d0cb19d9c9d836a1fffd7
Chou <hoangminhchaucp54@gmail.com>
2021-05-13 16:18:52 +0200

    Final commit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello World');
+const URL = "https://type.fit/api/quotes";
+
+fetch(URL)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    mainFunc(data);
+});
+
+function mainFunc(data) {
+    let button = document.getElementById("quoteButton");
+    button.addEventListener("click", function() {
+        let display = document.getElementById("quoteDisplay");
+        const index = Math.floor(Math.random() * data.length);
+        display.innerText = `${data[index].text} - ${data[index].author}`;
+    });
+}
